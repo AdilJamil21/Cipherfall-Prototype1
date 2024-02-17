395f6810cfceb59ac472adefdb4818b17a4f40f1
AdilJamil21 <adiljamil2000@gmail.com>
2024-02-17 23:59:51 +0500

    CameraMovement and PlayerMovement FIX

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private FixedJoystick _joystick;
-    [SerializeField] private Animator _animator;
-    [SerializeField] private float _movespeed;
+    [SerializeField] private FixedJoystick joystick;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float moveSpeed;
 
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector3(_joystick.Horizontal * _movespeed, _rigidbody.velocity.y, _joystick.Vertical * _movespeed );
+        // Get the input values from the joystick
+        float horizontalInput = -joystick.Horizontal;
+        float verticalInput = -joystick.Vertical;
 
-        if (_joystick.Horizontal != 0 ||  _joystick.Vertical != 0 )
+        // Calculate the movement direction based on joystick input
+        Vector3 movementDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        // Apply movement speed to the movement direction
+        Vector3 movement = movementDirection * moveSpeed;
+
+        // Apply the movement to the Rigidbody velocity
+        _rigidbody.velocity = new Vector3(movement.x, _rigidbody.velocity.y, movement.z);
+
+        // Rotate the player to face the movement direction
+        if (movementDirection != Vector3.zero)
         {
-            transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
+            transform.rotation = Quaternion.LookRotation(movementDirection);
         }
+
+        //Debug.Log(horizontalInput + " " + verticalInput);
 
     }
 }
